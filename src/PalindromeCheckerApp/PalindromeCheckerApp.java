c4b02fce92c1a25856833c3eeab0c06dba97e4ba
Rashal <rashaljeet766@gmail.com>
2026-03-02 16:11:37 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp/PalindromeCheckerApp.java
@@ -1,30 +1,32 @@
-public class UseCase9PalindromeCheckerApp {
+public class UseCase10PalindromeCheckerApp {
     public static void main(String[] args) {
 
         // Define input string
-        String input = "madam";
+        String input = "Never Odd Or Even";
 
-        // Call recursive method
-        boolean isPalindrome = check(input, 0, input.length() - 1);
+        // Normalize string (lowercase + remove spaces)
+        String normalized = input.toLowerCase().replaceAll("\\s+", "");
+
+        // Assume palindrome
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        // Compare characters
+        while (start < end) {
+
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
 
         // Display result
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
-    }
-
-    private static boolean check(String s, int start, int end) {
-
-        // Base condition
-        if (start >= end) {
-            return true;
-        }
-
-        // If mismatch
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
-
-        // Recursive call
-        return check(s, start + 1, end - 1);
     }
 }
